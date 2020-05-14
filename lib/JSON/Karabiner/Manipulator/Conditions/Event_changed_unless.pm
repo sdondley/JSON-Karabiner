e5377024d0519c4e2e8413d26710602b4365f4fa
Steve Dondley <s@dondley.com>
2020-05-13 23:44:40 -0400

    add missing conditions

---
+++ lib/JSON/Karabiner/Manipulator/Conditions/Event_changed_unless.pm
@@ -0,0 +1,68 @@
+package JSON::Karabiner::Manipulator::Conditions::Event_changed_unless ;
+
+use strict;
+use warnings;
+use JSON;
+use Carp;
+use parent 'JSON::Karabiner::Manipulator::Conditions::Event_changed_if';
+
+# ABSTRACT: definition for event_changed_unless condition
+
+1;
+
+__END__
+
+=head1 OVERVIEW
+
+Provide overview of who the intended audience is for the module and why it's useful.
+
+=head1 SYNOPSIS
+
+  use JSON::Karabiner;
+
+=head1 DESCRIPTION
+
+=method method1()
+
+
+
+=method method2()
+
+
+
+=func function1()
+
+
+
+=func function2()
+
+
+
+=attr attribute1
+
+
+
+=attr attribute2
+
+
+
+#=head1 CONFIGURATION AND ENVIRONMENT
+#
+#JSON::Karabiner requires no configuration files or environment variables.
+
+
+=head1 DEPENDENCIES
+
+=head1 AUTHOR NOTES
+
+=head2 Development status
+
+This module is currently in the beta stages and is actively supported and maintained. Suggestion for improvement are welcome.
+
+- Note possible future roadmap items.
+
+=head2 Motivation
+
+Provide motivation for writing the module here.
+
+#=head1 SEE ALSO
