c298ca61caf139604085c5e2703d4d71672c6442
Steve Dondley <s@dondley.com>
2020-05-15 00:04:47 -0400

    stop croaks when default directory doesn't exist during testing

--- lib/JSON/Karabiner.pm
+++ lib/JSON/Karabiner.pm
@@ -31,7 +31,7 @@ sub new {
     if ($opts->{_mod_file_dir}) {
       croak "The directory you attempted to set with the 'mod_file_dir' option does not exist.\n\n";
     } else {
-      croak "The default directory for storing complex modifications does not exist. Do you have Karbiner-Elements installed? Is it installed with a non-standard configuration? Try setting the location of the directory manually with the 'mod_file_dir' option. Consult this module's documentation for more information with using the 'perldoc JSON::Karabiner' command in the terminal.\n\n";
+      croak "The default directory for storing complex modifications does not exist. Do you have Karbiner-Elements installed? Is it installed with a non-standard configuration? Try setting the location of the directory manually with the 'mod_file_dir' option. Consult this module's documentation for more information with using the 'perldoc JSON::Karabiner' command in the terminal.\n\n" unless $ENV{TEST_HARNESS};
     }
   }
   bless $self, $class;
