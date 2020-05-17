da720e271930124e31d3aa25772cb2a3def87cee
Steve Dondley <s@dondley.com>
2020-05-17 13:46:12 -0400

    fix bugs in test script

--- xt/karabiner_generator-scripts/4_finger_swipes.pl
+++ xt/karabiner_generator-scripts/4_finger_swipes.pl
@@ -33,3 +33,36 @@ add_action        'to_delayed_if_canceled';
 add_set_variable  'left_shift pressed', '0';
 
 write_file();
+
+
+new_manipulator 'Double tap right-shift to swipe left', '4_finger_swipes.json';
+
+add_action    'from';
+add_key_code  'right_shift';
+
+add_action    'to';
+add_key_code  'right_arrow';
+add_modifiers 'shift', 'control';
+
+add_condition 'variable_if';
+add_variable  'right_shift pressed', 1;
+
+write_file();
+
+
+new_manipulator 'Double tap right-shift to swipe left', '4_finger_swipes.json';
+
+add_action        'from';
+add_key_code      'right_shift';
+
+add_action        'to';
+add_set_variable  'right_shift pressed', '1';
+add_key_code      'right_shift';
+
+add_action        'to_delayed_if_invoked';
+add_set_variable  'right_shift pressed', '0';
+
+add_action        'to_delayed_if_canceled';
+add_set_variable  'right_shift pressed', '0';
+
+write_file();
